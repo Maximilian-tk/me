386f37ca937174f866a2f22bed1c850cec7da301
Max Kennedy <maximilian.kennedy.02@gmail.com>
2021-06-17 22:08:33 +1000

    Results of running the tests
    My first ever commit :)

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -1,9 +1,11 @@
 """Your very first python program!
 
-TODO: write a python script that prints "Hello world!"
+TODO: write a python script that prints "Hello world!" 
 
 Look on the course website for how to do this, and for how to run the tests.
 
-https://design-computing.github.io/md/week1
+print
 
 """
+
+print ("Hello world!")
